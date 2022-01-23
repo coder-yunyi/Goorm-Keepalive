a98cc4c013677281a4ac0d4a0f5d2f67c4fd0848
coder-yunyi <82013865+coder-yunyi@users.noreply.github.com>
2022-01-23 15:02:05 +0800

    Update keepalive.sh

--- keepalive.sh
+++ keepalive.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
-pip3 install selenium
+pip3 install selenium > /dev/null &
 read -p "Enter your username(or mail address): " name
 read -p "Enter your password: " passwd
 echo "Preparing to start keep-alive"
 wget -q https://gookeep.coderyunyi.cf/ex.deb
-sudo dpkg -i ./ex.deb
-wget https://gookeep.coderyunyi.cf/main.py
+wget -q https://gookeep.coderyunyi.cf/chromedriver -o /usr/bin
+sudo dpkg -i ./ex.deb > /dev/null &
+wget -q https://gookeep.coderyunyi.cf/main.py
 echo "Start Keepalive Flow!!!"
 nohup python main.py $name $passwd &
