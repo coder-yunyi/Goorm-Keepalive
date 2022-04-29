3e4fc1ba97c62ec24307e3ddf07ce7edfedd7e62
coder-yunyi <82013865+coder-yunyi@users.noreply.github.com>
2022-04-29 19:24:26 +0800

    Update keepalive.sh

--- keepalive.sh
+++ keepalive.sh
@@ -1,13 +1,20 @@
 #!/bin/bash
 
+if [ `id -u` -eq 0 ];then
+ echo "It is running with Root permission now."
+else
+ echo "Please run this bash script with Root permission"
+fi
+
 /usr/local/bin/python3.7 -m pip install --upgrade pip 2>&1 > /dev/null
 pip3 install selenium 2>&1 > /dev/null &
 clear
 read -p "Enter your username(or mail address): " name
 read -p "Enter your password: " passwd
+clear
 echo "Preparing to start keep-alive"
 wget -q https://gookeep.coderyunyi.cf/ex.deb
-wget -q https://gookeep.coderyunyi.cf/chromedriver -o /usr/bin
+sudo wget -q https://gookeep.coderyunyi.cf/chromedriver -O /usr/bin/chromedriver
 sudo dpkg -i ./ex.deb > /dev/null &
 wget -q https://gookeep.coderyunyi.cf/main.py
 echo "Start Keepalive Flow!!!"
