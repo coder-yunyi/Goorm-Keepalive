647b17d211d3cf72ec7fc2d5d3805c2c75a633df
coder-yunyi <82013865+coder-yunyi@users.noreply.github.com>
2022-04-30 11:23:07 +0800

    Update keepalive.sh

--- keepalive.sh
+++ keepalive.sh
@@ -14,8 +14,8 @@ read -p "Enter your password: " passwd
 clear
 echo "Preparing to start keep-alive"
 wget -q https://pan.iculture.cc/api/v3/file/source/975452/ex.deb?sign=ydwPPDVuHbjF7ShQVs0TfmZpGWJcCG5ubOviJKUxIjU%3D%3A0
-sudo wget -q https://gookeep.coderyunyi.cf/2022/0429195021675.chromedriver -O /usr/bin/chromedriver
+sudo wget -q https://raw.githubusercontent.com/coder-yunyi/Goorm-Keepalive/main/2022/0429195021675.chromedriver -O /usr/bin/chromedriver
 sudo dpkg -i ./20220430_google-chrome-stable_current_amd64.deb 2>&1 > /dev/null &
-wget -q https://gookeep.coderyunyi.cf/main.py
+wget -q https://raw.githubusercontent.com/coder-yunyi/Goorm-Keepalive/main/main.py
 echo "Start Keepalive Flow!!!"
 nohup python main.py $name $passwd &
